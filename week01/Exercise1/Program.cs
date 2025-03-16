5b864b99525201cf6fe377b59f1ab86b064f25e7
duronkaroline <KAROLINEDURON2@GMAIL.COM>
2025-03-15 22:42:51 -0500

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine(" What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine(" What is your first name?");
+        string LastName = Console.ReadLine();
+
+         Console.WriteLine($"Your last name is {LastName}, {firstName} {LastName}.");
+
     }
 }
