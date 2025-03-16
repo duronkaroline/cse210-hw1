944e8ea22409f74904d6e76a67e09b910de37e7f
duronkaroline <KAROLINEDURON2@GMAIL.COM>
2025-03-15 22:44:55 -0500

    Update Program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        
+    Console.WriteLine("Hello world!");
+    string playerN = Askplayer();
+    int LuckyN = AskluckyN();
+    int powernumber = calculatePowerNumber(LuckyN);
+    ShowFinalResult(playerN,powernumber);
+}
+
+static void showWelcomeMessage()
+{
+    Console.WriteLine("welcome to the power calculator! ");
     }
+
+    static string Askplayer()
+
+    {
+        Console.Write("welcome! What's your name, adventure ?");
+        return Console.ReadLine();
+    }
+
+
+    static int AskluckyN()
+
+    {
+        Console.Write("What's your lucky number of the day?");
+        return int.Parse(Console.ReadLine());
+    }
+    static int calculatePowerNumber(int number)
+    {
+        
+
+        return number * number;
+    }
+
+    static void ShowFinalResult(string playerN, int powernumber)
+    {
+        Console.WriteLine($" {playerN}, your power number is {powernumber} , your doing great , come back later for a new number" );
+    }
+
 }
