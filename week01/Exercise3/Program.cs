202b621f240a9ac45f8fb1b66951c5a1445adad0
duronkaroline <KAROLINEDURON2@GMAIL.COM>
2025-03-15 22:44:00 -0500

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        bool Playagain = true;
+
+        while (Playagain)
+        { 
+            int magicNumberr =random.Next(1,100);
+            int guess = 0;
+            int attempts = 0;
+            Console.WriteLine("  Pick a number between 1-20");
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+            if (guess < magicNumberr)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess> magicNumberr)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine($" you guessed it! your attempts :{attempts} ");
+            }
     }
+}
 }
