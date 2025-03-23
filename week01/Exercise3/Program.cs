f8bc53e9c3ea6fff127b2c8824efe7b68e88dae9
duronkaroline <KAROLINEDURON2@GMAIL.COM>
2025-03-23 00:25:35 -0500

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,33 +1,55 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Random random = new Random();
-        bool Playagain = true;
+        
+        var scriptures = new List<(string reference, string text)>
+    {
+         ("Psalm 119:105", "Your word is a lamp to my feet and a light to my path."),
+            ("Psalm 23:1", "The Lord is my shepherd; I shall not want."),
+            ("Romans 8:28", "And we know that all things work together for good to them that love God, to them who are the called according to his purpose."),
+            ("Romans 12:2", "Do not be conformed to this world, but be transformed by the renewing of your mind, that you may prove what is the good and acceptable and perfect will of God.")    
+    
+      };
 
-        while (Playagain)
-        { 
-            int magicNumberr =random.Next(1,100);
-            int guess = 0;
-            int attempts = 0;
-            Console.WriteLine("  Pick a number between 1-20");
-            Console.WriteLine("What is your guess?");
-            guess = int.Parse(Console.ReadLine());
-            attempts++;
-            if (guess < magicNumberr)
+        Random random = new Random();
+        var randomScripture = scriptures[random.Next(scriptures.Count)];
+
+       
+              Scripture scripture = new Scripture(randomScripture.reference, randomScripture.text);
+
+      
+        scripture.DisplayScripture();
+
+        
+        while (true)
+        {
+            Console.WriteLine("Press Enter to hide words or type 'quit' to exit.");
+                  string input = Console.ReadLine().Trim().ToLower();
+
+            if (input == "quit")
             {
-                Console.WriteLine("Higher");
+                break;
             }
-            else if (guess> magicNumberr)
+
+            
+            scripture.HideRandomWord();
+            Console.Clear();
+
+            
+            scripture.DisplayScripture();
+
+          
+            if (scripture.AllWordsHidden())
             {
-                Console.WriteLine("lower");
+                Console.WriteLine("All words are hidden. The scripture is fully memorized!");
+                break;
             }
-            else
-            {
-                Console.WriteLine($" you guessed it! your attempts :{attempts} ");
-            }
+            
+        }
     }
+    
 }
-}
+
